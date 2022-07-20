6019d95243a5db8990e01869a359b287d4f36791
Piotr Tańcula <tanculapiotr@gmail.com>
2022-07-20 17:01:13 +0200

    send the whole app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import FormattedTime from "./components/FormattedTime/FormattedTime ";
+import Button from "./components/Button/Button";
+import styles from './App.module.scss';
+import { useEffect } from "react";
 
-function App() {
+const App = () => {
+
+  const [currentTime, setTime] = useState(0);
+  const [timer, setTimer] = useState(null);
+
+  const startTimer = () => {
+    setTimer(
+      setInterval(() => {
+        setTime((currentTime) => currentTime + 1);
+      }, 1)
+    );
+  };
+
+  const stopTimer = () => {
+    if (timer) {
+      clearInterval(timer);
+    }
+  };
+
+  const resetTimer = () => {
+    setTime(0);
+  };
+
+  useEffect(() => {
+    return () => {
+       if(timer) clearInterval(timer);
+    };
+  }, []);
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.App}>
+      <FormattedTime time={currentTime} className={styles.FormattedTime} />
+      <div className={styles.App}>
+        <Button onClick={startTimer}>Start</Button>
+        <Button onClick={stopTimer}>Stop</Button>
+        <Button onClick={resetTimer}>Reset</Button>
+      </div>
     </div>
   );
 }
